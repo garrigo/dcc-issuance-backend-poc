440948ee1fd525a0cab7af3a49c40a8ad613e0bd
garrigo <33257047+garrigo@users.noreply.github.com>
2022-03-05 19:25:01 +0100

    Custom cose

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -15,9 +15,12 @@ def sign():
 
     if request.method == 'POST':
         today = int(time.mktime(time.strptime(str(date.today()), "%Y-%m-%d")))
+        dob = int(time.mktime(time.strptime(request.form['dob'], "%Y-%m-%d")))
         cert_type = request.form['type']
+        
 
         if (cert_type == 'v'):
+            vax_date = int(time.mktime(time.strptime(request.form['dt'], "%Y-%m-%d")))
             payload = {
                 4: today+YEAR_IN_SECONDS,
                 6: today,
@@ -31,7 +34,8 @@ def sign():
                                 "ma": request.form['ma'],
                                 "dn": int(request.form['dn']),
                                 "sd": int(request.form['sd']),
-                                "dt": request.form['dt'],
+                                # "dt": request.form['dt'],
+                                "dt": vax_date
                             }
                         ],
                         "nam": {
@@ -41,7 +45,8 @@ def sign():
                             "gn": request.form['gn'],
                         },
                         "ver": request.form['ver'],
-                        "dob": request.form['dob'],
+                        "dob": dob,
+                        # "dob": request.form['dob'],
                     }
                 }
             }
@@ -68,7 +73,8 @@ def sign():
                             "gn": request.form['gn'],
                         },
                         "ver": request.form['ver'],
-                        "dob": request.form['dob'],
+                        "dob": dob,
+                        # "dob": request.form['dob'],
                     }
                 }
             }
@@ -94,14 +100,15 @@ def sign():
                             "gn": request.form['gn'],
                         },
                         "ver": request.form['ver'],
-                        "dob": request.form['dob'],
+                        "dob": dob,
+                        # "dob": request.form['dob'],
                     }
                 }
             }
         else:
             render_template('index.html', generated=False)
 
-        base45_data = sign_GP(payload, 0)
+        base45_data = sign_newcose(payload, 0)
 
         return render_template('index.html',
                                 generated=True,
--- app/func/__init__.py
+++ app/func/__init__.py
@@ -1,5 +1,6 @@
 import zlib
 import base45
+import math
 from cwt import COSEKey
 import cbor_json
 from cose.messages import Sign1Message
@@ -12,18 +13,86 @@ from cose.keys.keytype import KtyEC2
 from cose.keys.keyops import SignOp, VerifyOp
 
 
-def store_cert(certificate):
-    return 0
+from ecdsa import SigningKey, VerifyingKey
 
+class NeoCBOR:
+    def __init__(self, payload_dict):
+        #algorithm used
+        self.payload = '01'
+        #kid
+        self.payload = self.payload + (1).to_bytes(2, byteorder='big').hex()
+        #tag for number of bytes used by unix time integer
+        hex_temp = (hex(payload_dict[4]))[2:]
+        self.payload = self.payload + (len(hex_temp)//2).to_bytes(1, byteorder='big').hex()
+        #unix date
+        self.payload = self.payload + hex_temp
+        #tag for number of bytes used by unix time integer
+        hex_temp = (hex(payload_dict[6]))[2:]
+        self.payload = self.payload + (len(hex_temp)//2).to_bytes(1, byteorder='big').hex()
+        #unix date
+        self.payload = self.payload + hex_temp
+        #tag for bytes used by surname
+        hex_temp = (payload_dict[-260][1]["nam"]["fn"]).encode('utf-8').hex()
+        self.payload = self.payload + (len(hex_temp)//2).to_bytes(1, byteorder='big').hex()
+        self.payload = self.payload + hex_temp
+        #tag for bytes used by name
+        hex_temp = (payload_dict[-260][1]["nam"]["gn"]).encode('utf-8').hex()
+        self.payload = self.payload + (len(hex_temp)//2).to_bytes(1, byteorder='big').hex()
+        self.payload = self.payload + hex_temp
+        #tag for number of bytes used by unix time integer
+        hex_temp = (hex(payload_dict[-260][1]["dob"]))[2:]
+        self.payload = self.payload + (len(hex_temp)//2).to_bytes(1, byteorder='big').hex()
+        self.payload = self.payload + hex_temp
+        #type of certificate
+        self.payload = self.payload + (1).to_bytes(1, byteorder='big').hex()
+        if 1:
+            #disease targeted & num
+            self.payload = self.payload + (10).to_bytes(1, byteorder='big').hex()
+            self.payload = self.payload + (200).to_bytes(1, byteorder='big').hex()
+            #vaccine used
+            self.payload = self.payload + (500).to_bytes(2, byteorder='big').hex()
+            #doses done and expected
+            self.payload = self.payload + (payload_dict[-260][1]["v"][0]["sd"]).to_bytes(1, byteorder='big').hex()
+            self.payload = self.payload + (payload_dict[-260][1]["v"][0]["dn"]).to_bytes(1, byteorder='big').hex()
+            #date of vaccine
+            hex_temp = (hex(payload_dict[-260][1]["dob"]))[2:]
+            self.payload = self.payload + (len(hex_temp)//2).to_bytes(1, byteorder='big').hex()
+            self.payload = self.payload + hex_temp
+
+    
+
+def sign_newcose(payload_dict, qualcosa=0):
+    with open("./app/certs/private.pem") as key_file:
+        private_key = SigningKey.from_pem(key_file.read())
+
+    with open("./app/certs/public.pem") as key_file:
+        public_key = VerifyingKey.from_pem(key_file.read())
+
+    neocbor = NeoCBOR(payload_dict)
+    signature = private_key.sign(bytes.fromhex(neocbor.payload))
+    try:
+        print(public_key.verify(signature, bytes.fromhex(neocbor.payload)))
+    except:
+        print(False)
+    full_payload = signature.hex() + neocbor.payload
+    print(neocbor.payload)
+    zlib_data = zlib.compress(bytes.fromhex(full_payload)) 
+    base45_data = base45.b45encode(zlib_data)
+    base45_data = base45_data.decode('utf-8')
+    # print(len(base45_data))
+    return base45_data
+
+
+    
 
 def sign_GP(payload_dict, kid_int):
 
+    #payload of the DCC get passed to the 
     payload = cbor_json.cbor_from_native(payload_dict)
     kid = kid_int.to_bytes(2, 'big')
     
     with open("./app/certs/private.pem") as key_file:
-        private_key = COSEKey.from_pem(key_file.read())
-    
+        private_key = COSEKey.from_pem(key_file.read()) 
     # # print(vars(private_key))
 
     cose_key ={
@@ -43,7 +112,9 @@ def sign_GP(payload_dict, kid_int):
 
     cose_hex = bytes.fromhex(encoded.hex())
     # print(cose_hex.hex())
-    zlib_data = zlib.compress(cose_hex) 
+    zlib_data = zlib.compress(b'4BCB8A4680476BBECFB3C9D7B7E14402164E016E6D4D7CD8A221DBDA9205044F3654F9C17C2D4F03BDF24BA55755D374B381976439D7960406399A46D404A466') 
+    # zlib_data = zlib.compress(cose_hex) 
     base45_data = base45.b45encode(zlib_data)
     base45_data = "HC1:"+base45_data.decode('utf-8')
+    # print(len(base45_data))
     return base45_data
